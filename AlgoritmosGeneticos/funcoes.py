cd6f51468973512981f080a857bd527b6f95b6b3
Pedro Thomazelli Ferreira <pedro220058@ilum.cnpem.br>
2023-03-09 17:41:30 -0300

    Adição do experimento A.02

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,1 +1,10 @@
- 
+def funcao_objetivo_cb(individuo):
+    """ Computa a função objetivo no problema das cb.
+    
+    Args:
+        indivíduo: lista contendo os genes da cb.
+    
+    Return:
+        Um valor que representa as somas
+    """
+    return sum(individuo) 
