2bc16b3440ad8a614344c805a413641c872ee457
Pedro Thomazelli Ferreira <pedro220058@ilum.cnpem.br>
2023-03-16 15:06:00 -0300

    Atualização de funcoes.py e experimento A.01

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,3 +1,31 @@
+import random
+
+
+def gene_cb ():
+    """ Gerar um gene válido para o problema apresentado
+    
+    Return:
+        Valor 0 ou 1.
+    """
+    lista = [0,1]
+    gene = random.choice(lista)
+    return gene
+
+def individuo_cb(n):
+    """ Gerar um indivíduo para o problema apresentado.
+    
+    Args:
+        n: número de genes do indivíduo.
+    
+    Return:
+        lista com "n" genes, com valor 0 ou 1.
+    """
+    individuo = []
+    for i in range(n):
+        gene = gene_cb()
+        individuo.append(gene)
+    return individuo
+
 def funcao_objetivo_cb(individuo):
     """ Computa a função objetivo no problema das cb.
     
